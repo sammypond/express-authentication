dac095aad503c337862d73faecfcaeff49cc547c
Samuel Pond Marritz <kim@Samuels-MacBook-Air.local>
2019-06-27 16:08:02 -0700

    added session and configuration

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
+const session = require('express-session');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -9,6 +10,13 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 app.use(ejsLayouts);
+
+// Configures express-session middleware
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false, 
+  saveUninitialized: true
+}));
 
 app.get('/', function(req, res) {
   res.render('index');
