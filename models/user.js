100c8a90267f26895e4d95af551e014dcd39e5c7
Samuel Pond Marritz <kim@Samuels-MacBook-Air.local>
2019-06-27 15:07:09 -0700

    first commit

---
+++ models/user.js
@@ -0,0 +1,49 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define('user', {
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'Invalid email address'
+        }
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [1,99],
+          msg:'Invalid username, must be between 1 and 99 characters.'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8,99],
+          msg: 'Password must be at least 8 character.'
+        }
+      }
+    }
+  }, {
+    hooks: {
+      beforeCreate: function(pendingUser, options){
+        if (pendingUser && pendingUser.password){
+          var hash = bcrypt.hashSync(pendingUser.password, 12);
+          pendingUser.password = hash;
+        }
+      }
+    }
+  });
+  user.associate = function(models) {
+    // associations can be defined here
+  };
+  user.prototype.validPassword = function(passwordTyped){
+    return bcrypt.compareSync(passwordTyped, this.password);
+  };
+  return user;
+};
